26768fb117a1da309c78efb203b499de200c75b9
Artem Gilmudinov <guliash92@gmail.com>
2016-04-28 21:29:22 +0300

    simple support alert;

--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,12 +1,61 @@
 package com.github.guliash.androidexplorer;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
+
+    private Button mSimpleBtn, mFragmentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSimpleBtn = (Button)findViewById(R.id.simple);
+        mFragmentBtn = (Button)findViewById(R.id.fragment);
+
+        mSimpleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                        .setCancelable(true)
+                        .setTitle(R.string.simple_alert)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.e(TAG, "SIMPLE POSITIVE");
+                            }
+                        }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.e(TAG, "SIMPLE NEGATIVE");
+                            }
+                        }).setNeutralButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.e(TAG, "SIMPLE NEUTRAL");
+                            }
+                        }).setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                                Log.e(TAG, "SIMPLE DISMISS");
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+        mFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
