47ac985ae2ddd8fef6b095b0945f41b621ea34c7
Artem <guliash92@gmail.com>
2016-04-29 16:12:30 +0300

    hot observable?

--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -3,11 +3,14 @@ package com.github.guliash.androidexplorer;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
+import rx.observables.ConnectableObservable;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
@@ -17,6 +20,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         test();
+        test2();
     }
 
     static class A {
@@ -43,10 +47,12 @@ public class MainActivity extends BaseActivity {
     }
 
     public void test() {
+
         Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 int size = 10;
+                Log.e(TAG, "SUBSCRIBE THREAD NAME " + Thread.currentThread().getName());
                 for(int i = 0; i < size; i++) {
                     subscriber.onNext(String.valueOf(i));
                 }
@@ -55,6 +61,7 @@ public class MainActivity extends BaseActivity {
         }).map(new Func1<String, A>() {
             @Override
             public A call(String s) {
+                Log.e(TAG, "MAP THREAD NAME " + Thread.currentThread().getName());
                 return A.parse(s);
             }
         }).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<A>() {
@@ -70,8 +77,41 @@ public class MainActivity extends BaseActivity {
 
             @Override
             public void onNext(A a) {
+                Log.e(TAG, "NEXT THREAD NAME " + Thread.currentThread().getName());
                 Log.e(TAG, "NEXT " + a);
             }
         });
+    }
+
+    public void test2() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ConnectableObservable< Long > observable = Observable.interval(1, TimeUnit.SECONDS).publish();
+                observable.connect();
+                try {
+                    Thread.sleep(10000);
+                } catch (Exception e) {
+
+                }
+                observable.subscribe(new Observer<Long>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.e(TAG, "BOOM " + aLong);
+                    }
+                });
+            }
+        }).start();
+
     }
 }
