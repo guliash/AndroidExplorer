96c52547cdeae05d8f6d80efcff8e52c1825071a
Artem <guliash92@gmail.com>
2016-05-13 16:42:26 +0300

    asyncs

--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.guliash.androidexplorer;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends BaseActivity {
 
@@ -9,5 +11,42 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        asyncTask.execute(new Void[]{null});
+        asyncTask2.execute(new Void[]{null});
     }
+
+    AsyncTask<Void, Void, Void> asyncTask2 = new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void...params) {
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.e(TAG, "ON POST 1");
+        }
+    };
+
+    AsyncTask<Void, Void, Void> asyncTask = new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void...params) {
+            try {
+                Thread.sleep(1_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.e(TAG, "ON POST 2");
+        }
+    };
+
 }
