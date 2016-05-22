7c9493c6b4ef3126ab32746ce5fdc4eeb292551e
Artem Gilmudinov <guliash92@gmail.com>
2016-05-22 11:23:05 +0300

    simple splash example;

--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,13 +1,51 @@
 package com.github.guliash.androidexplorer;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.logo)
+    public ImageView imageView;
+
+    private boolean mShouldFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startSecondActivity();
+            }
+        }, 3000);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mShouldFinish) {
+            finish();
+        }
+    }
+
+    private void startSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, imageView, "logo");
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+        mShouldFinish = true;
     }
 }
---
+++ app/src/main/java/com/github/guliash/androidexplorer/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.github.guliash.androidexplorer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SecondActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
