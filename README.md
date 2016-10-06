4ed17d88e31d6b613dfafd8697591db8170ceae1
guliash <guliash92@gmail.com>
2016-10-06 16:58:49 +0300

    view.animate() at the same time;

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AndroidExplorer
 This project contains some simple examples of using Android features
+
+If we run view.animate() more than once and there are running animations, then they will be executing at the same time.
+Use view.animate().cancel() to cancel them.
--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,13 +1,37 @@
 package com.github.guliash.androidexplorer;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.go_button)
+    Button mGoButton;
+
+    private boolean mFlag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.go_button)
+    void onClick() {
+        mFlag = !mFlag;
+        if(mFlag) {
+            mGoButton.animate().cancel();
+            mGoButton.animate().translationX(1000)
+                    .setDuration(30000);
+        } else {
+            mGoButton.animate().cancel();
+            mGoButton.animate().scaleX(0.5f).scaleY(0.5f)
+                    .setDuration(10000);
+        }
     }
 }
