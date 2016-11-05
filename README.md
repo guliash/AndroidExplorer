ea613fe5fff95b27c8f9b9e587e5146e3d54af34
Artem Gilmudinov <guliash92@gmail.com>
2016-11-05 21:54:32 +0300

    this is activity;

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AndroidExplorer
 This project contains some simple examples of using Android features
+
+In lambda 'this' is the outer object.
--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,13 +1,23 @@
 package com.github.guliash.androidexplorer;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.btn)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
+        button.setOnClickListener((view) -> {
+            this.button = null;
+        });
     }
 }
