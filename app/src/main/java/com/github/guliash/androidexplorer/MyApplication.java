e50bcf0d8ceb3fea4b252dee267918a5f0aebb3b
Artem Gilmudinov <guliash92@gmail.com>
2016-12-07 16:52:14 +0300

    planted debug tree;

---
+++ app/src/main/java/com/github/guliash/androidexplorer/MyApplication.java
@@ -0,0 +1,14 @@
+package com.github.guliash.androidexplorer;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}
