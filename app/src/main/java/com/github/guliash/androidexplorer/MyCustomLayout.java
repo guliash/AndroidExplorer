6fca452d15346e6702c4cb139632f0b37f855e4c
Artem Gilmudinov <guliash92@gmail.com>
2016-12-24 20:51:51 +0300

    check whether addview get called;

--- app/src/main/java/com/github/guliash/androidexplorer/MyCustomLayout.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MyCustomLayout.java
@@ -3,7 +3,11 @@ package com.github.guliash.androidexplorer;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import timber.log.Timber;
 
 public class MyCustomLayout extends LinearLayout {
 
@@ -20,4 +24,9 @@ public class MyCustomLayout extends LinearLayout {
         LayoutInflater.from(context).inflate(R.layout.inflated, this);
     }
 
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        Timber.d("add view");
+        super.addView(child, index, params);
+    }
 }
