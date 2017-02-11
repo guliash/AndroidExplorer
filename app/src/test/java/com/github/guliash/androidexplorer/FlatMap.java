ff77d834dab936618de523909b3b4b1f14c8418d
Artem Gilmudinov <guliash92@gmail.com>
2017-02-11 14:50:52 +0300

    wip;

--- app/src/test/java/com/github/guliash/androidexplorer/FlatMap.java
+++ app/src/test/java/com/github/guliash/androidexplorer/FlatMap.java
@@ -1,9 +1,10 @@
 package com.github.guliash.androidexplorer;
 
-import org.junit.*;
+import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import rx.Notification;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
@@ -22,9 +23,52 @@ public class FlatMap {
                     System.out.println(Thread.currentThread().getName());
                 })
                 .take(10)
-        .subscribe(subscriber);
+                .subscribe(subscriber);
 
         subscriber.awaitTerminalEvent();
+    }
+
+
+    @Test
+    public void test() {
+
+        PrintSubscriber<Object> subscriber = new PrintSubscriber<>();
+
+        Observable.concat(Observable.just(1), Observable.empty())
+                .materialize().subscribe(subscriber);
+
+        Observable.error(new Throwable())
+                .materialize()
+                .doOnNext(ø -> System.out.println("doOnNext " + Thread.currentThread().getName()))
+                .flatMap(notification -> {
+                    if (notification.getKind() == Notification.Kind.OnNext) {
+                        return Observable.just(notification.getValue());
+                    }
+                    if (notification.getKind() == Notification.Kind.OnError) {
+                        return Observable.just(1)
+                                .doOnNext(v -> System.out.println("Inner onNext " + Thread.currentThread().getName()))
+                                .observeOn(Schedulers.computation())
+                                .doOnSubscribe(() -> {
+                                    System.out.println("Ui here " + Thread.currentThread().getName());
+                                });
+                    }
+                    return Observable.empty();
+                })
+                .subscribeOn(Schedulers.io())
+                .doOnNext(value -> System.out.println("Outer doOnNext " + Thread.currentThread().getName()))
+                .subscribe(subscriber);
+
+        subscriber.awaitTerminalEvent();
+    }
+
+    @Test
+    public void test1() {
+        PrintSubscriber printSubscriber = new PrintSubscriber();
+        Observable.just(1, 2)
+                .observeOn(Schedulers.io())
+                .flatMap(i -> Observable.just(1, 2).observeOn(Schedulers.computation()))
+                .doOnNext(ø -> System.out.println("current thread " + Thread.currentThread().getName()))
+                .subscribe(printSubscriber);
     }
 
 }
--- app/src/test/java/com/github/guliash/androidexplorer/Just.java
+++ app/src/test/java/com/github/guliash/androidexplorer/Just.java
@@ -2,15 +2,12 @@ package com.github.guliash.androidexplorer;
 
 import org.junit.Test;
 
-import rx.Observable;
-import rx.schedulers.Schedulers;
 
 public class Just {
 
     @Test
     public void just() {
         PrintSubscriber subscriber = new PrintSubscriber();
-        Observable.fromCallable(() -> Observable.just(1, 2, 3, getInt())).subscribeOn(Schedulers.io()).subscribe(subscriber);
 
         subscriber.awaitTerminalEvent();
     }
