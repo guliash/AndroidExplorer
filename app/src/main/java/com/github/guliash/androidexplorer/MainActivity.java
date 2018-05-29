cc9f26fed0fb3f308bf45bda49094a7a2a27ee22
Artem Gilmudinov <guliash92@gmail.com>
2018-05-29 12:06:44 +0300

    view stub example;

--- app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
+++ app/src/main/java/com/github/guliash/androidexplorer/MainActivity.java
@@ -1,13 +1,27 @@
 package com.github.guliash.androidexplorer;
 
 import android.os.Bundle;
+import android.view.ViewStub;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.stub)
+    ViewStub viewStub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        viewStub.inflate();
+
+        ButterKnife.bind(this);
+
+        System.out.println("debug " + viewStub);
     }
 }
